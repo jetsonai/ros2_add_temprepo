4a1a4b0eca750c8c9d5c90ed9892e1bed11ed277
R SSAEM <96120477+jetsonai@users.noreply.github.com>
2025-11-06 01:42:39 +0900

    Update lidar_sub_test.py

--- sensor_test_pack/sensor_test_pack/lidar_sub_test.py
+++ sensor_test_pack/sensor_test_pack/lidar_sub_test.py
@@ -3,34 +3,37 @@
 # Copyright 2024 JetsonAI CO., LTD.
 #
 # Author: Kate Kim
+# colcon build --packages-select sensor_test_pack
+# ros2 run sensor_test_pack lidar_sub_node
 
 import rclpy # Python library for ROS 2
 from rclpy.node import Node # Handles the creation of nodes
-from sensor_msgs.msg import LaserScan # 
-
+from sensor_msgs.msg import LaserScan 
+from rclpy.qos import qos_profile_sensor_data, QoSProfile
+ 
+ranges_list = []
  
 class LidarSubscriber(Node):
 
     def __init__(self):
         super().__init__('lidar_sub_node')
-
-        qos = 10
+        
+        qos = QoSProfile(depth=10)
+          
         self.subscription = self.create_subscription(
           LaserScan, 
           '/scan', 
           self.listener_callback, 
-          qos)
-
+          qos_profile=qos_profile_sensor_data)
         self.subscription # prevent unused variable warning
-      
 
+   
     def listener_callback(self, data):
-        self.get_logger().info('Receiving lidar frame')
-
+        #self.get_logger().info('Receiving lidar frame')
         global ranges_list
         ranges_list = data.ranges
-        bsafe = self.safeDistance(ranges_list)
-     
+        bSafe = self.safeDistance(ranges_list) 
+ 
     def safeDistance(self, ranges):
         bSafe = 0
         if( len(ranges) > 90) :
@@ -39,24 +42,32 @@ class LidarSubscriber(Node):
                 #print("ranges[{}] : {}".format(f, ranges[f]))
                 if(ranges[f] <= 0.2 and ranges[f] != 0.0):
                     bSafe = 0
-                    rospy.loginfo("<WARNING> ranges[{}] : {}".format(f, ranges[f]))
+                    self.get_logger().info("<WARNING> ranges[{}] : {}".format(f, ranges[f]))
                     break
             if(bSafe == 1):
                 for f in range(0,20):
                     #print("ranges[{}] : {}".format(f, ranges[f]))
                     if(ranges[f] <= 0.2 and ranges[f] != 0.0):
                         bSafe = 0
-                        rospy.loginfo("<WARNING> ranges[{}] : {}".format(f, ranges[f]))
+                        self.get_logger().info("<WARNING> ranges[{}] : {}".format(f, ranges[f]))
                         break
         print("bSafe:{}".format(bSafe))
         return bSafe
-    
+
+
+
+ 
 def main(args=None):
 
-  rclpy.init(args=args)
-  image_subscriber = LidarSubscriber() 
-  rclpy.spin(image_subscriber)
-  image_subscriber.destroy_node()
+    rclpy.init(args=args)
+    lidar_subscriber = LidarSubscriber() 
+    rclpy.spin(lidar_subscriber)
+    lidar_subscriber.destroy_node()
+    rclpy.shutdown()
+  
+if __name__ == '__main__':
+    main()
+ 
   rclpy.shutdown()
   
 if __name__ == '__main__':
